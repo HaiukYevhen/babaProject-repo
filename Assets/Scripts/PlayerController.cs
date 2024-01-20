87005212c3fc55fe70af585d713823bf7f1358c2
Ivan Haiuk <ivan.gauk.777@gmail.com>
2024-01-20 21:38:15 +0200

    Player movement fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,29 +8,27 @@ public class Player : MonoBehaviour
 
     private Rigidbody rb;
 
-    //public bool canMove = false;
-    // Start is called before the first frame update
     void Start()
     {
 		rb = GetComponent<Rigidbody>();
 	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Move(true);
-    }
-    public void Move(bool canMove)
-    {
-        if(canMove == true)
-        {
-            horizontalInput = Input.GetAxis("Horizontal");
-            verticalInput = Input.GetAxis("Vertical");
+	void Update()
+	{
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+	}
 
-			Vector3 position = rb.position;
-			position.x += horizontalInput * speed * Time.deltaTime;
-			position.z += verticalInput * speed * Time.deltaTime;
-			rb.MovePosition(position);
-        }
-    }
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+		Vector3 position = rb.velocity;
+		position.x = horizontalInput;
+		position.z = verticalInput;
+
+		if (position.magnitude > 1)
+			position.Normalize();
+
+		rb.velocity = position * speed;
+	}
 }
